fc87bddbd3e85f9a12ca5bc0439b39a5ece2a06f
Colin Marshall <marshallcolin71@gmail.com>
2017-08-20 16:26:11 -0400

    fixed some things

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,7 +1,10 @@
 (function () {
-    function HomeCtrl(Room, $uibModal) {
+    function HomeCtrl(Room, , Message, $uibModal) {
+        var home = this;
         this.rooms = Room.all;
-        this.addRoom = function() {
+        home.currentRoom = null;
+
+        home.addRoom = function() {
             $uibModal.open({
                 templateUrl: '/templates/modal.html',
                 size: 'sm',
@@ -9,10 +12,16 @@
             });
         }
 
+        home.setCurrentRoom = function (room) {
+            home.currentRoom = room;
+            home.messages = Message.getByRoomId(home.currentRoom.$id);
+            console.log(home.messages)
+        }
+
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 
 }) ();
