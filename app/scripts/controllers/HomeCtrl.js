74b58a8b428a91622325cf1ad86c6d95a3315a4f
Colin Marshall <marshallcolin71@gmail.com>
2017-08-20 16:08:56 -0400

    checkpoint 3 complete

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,11 +1,18 @@
 (function () {
-    function HomeCtrl(Room) {
+    function HomeCtrl(Room, $uibModal) {
         this.rooms = Room.all;
+        this.addRoom = function() {
+            $uibModal.open({
+                templateUrl: '/templates/modal.html',
+                size: 'sm',
+                controller: 'ModalCtrl as modal'
+            });
+        }
 
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', HomeCtrl);
+        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
 
 }) ();
